74468960287b0492afb1b40edd02b56a308ff5fe
Thomas Lucas <t.lucas@lienas.de>
2021-03-17 16:20:38 +0100

    add authentication with auth0

---
+++ src/main/java/com/osde/prepo/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.osde.prepo.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+@EnableResourceServer
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                .mvcMatchers(("/api/companies/**")).authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(resourceId);
+    }
+}
+
