f7af3f8d63a124dd5466fd49c403e053296ca907
thomas <t.lucas@lienas.de>
2021-03-17 08:22:57 +0100

    add validation

--- src/main/java/com/osde/prepo/entity/Company.java
+++ src/main/java/com/osde/prepo/entity/Company.java
@@ -1,22 +1,38 @@
 package com.osde.prepo.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Company {
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long companyId;
+
     private String ownerId; // Id of the user, who created the entry
+
+    @NotNull(message = "Please provide a name for the company")
     private String name; // Company Name
+
     private String city; // City where the company resides
     private String country; // Country where the company resides
+
     @Column(length = 500)
     private String profile; // short profile of the company
     private String logoUrl; // Url to logo
+
+    public Company(String ownerId, String name, String city, String country, String profile, String logoUrl) {
+        this.ownerId = ownerId;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.profile = profile;
+        this.logoUrl = logoUrl;
+    }
 }
