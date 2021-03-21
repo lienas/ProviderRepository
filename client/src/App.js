73b3b823231c423f42711f7e8285b4c886e8372c
thomas <t.lucas@lienas.de>
2021-03-21 18:52:57 +0100

    Material UI Card for Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,54 @@
-import {Container, createMuiTheme, ThemeProvider} from "@material-ui/core";
+import {
+    AppBar,
+    Box,
+    Container,
+    createMuiTheme,
+    makeStyles,
+    ThemeProvider,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
 import {brown, lime} from "@material-ui/core/colors";
 import {ProviderList} from "./components/ProviderList";
 
 const theme = createMuiTheme({
-      palette: {
-        primary: {
-          main: lime[800]
-        },
-        secondary: {
-          main: brown[300],
-        },
-      }
+        palette: {
+            primary: {
+                main: lime[800]
+            },
+            secondary: {
+                main: brown[300],
+            },
+        }
     }
 )
 
+const useStyles = makeStyles({
+        title: {
+            color: "white"
+        }
+    }
+);
+
+
 function App() {
-  return (
-      <ThemeProvider theme={theme}>
-          <Container>
-              <ProviderList/>
-          </Container>
-      </ThemeProvider>
-  );
+    const classes = useStyles()
+    return (
+        <ThemeProvider theme={theme}>
+            <AppBar>
+                <Toolbar>
+                    <Typography className={classes.title}>
+                        Provider-Repository
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <Box my={20}>
+                    <ProviderList/>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default App;
